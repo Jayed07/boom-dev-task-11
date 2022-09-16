b082cb8a8438d663fbaf051b45a53e490f951a92
Jayed07 <jayed.ws7@gmail.com>
2022-09-17 00:34:17 +0300

    selector fixed

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,10 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  const hotProductsList = document.querySelectorAll(".subtitle is-6 price hot");
+  const hotProductsList = document.querySelectorAll(".hot");
 
   hotProductsList.forEach((hotProduct) => {
     hotProduct.innerHTML += '🔥';
   });
 
-  
-  // 🔥
 });
