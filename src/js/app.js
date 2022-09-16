b6b3a1bf25c799091045ee3c80854d9f7345616c
Jayed07 <jayed.ws7@gmail.com>
2022-09-17 00:15:37 +0300

    js fixed

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,12 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
+  const hotProductsList = document.querySelectorAll(".subtitle is-6 price hot");
+
+  hotProductsList.forEach((hotProduct) => {
+    hotProduct.innerHTML += '🔥';
+  });
+
+  
+  // 🔥
 });
